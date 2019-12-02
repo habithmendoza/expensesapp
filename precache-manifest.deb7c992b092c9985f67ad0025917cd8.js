3d241a9779913927ce5c717ca7d54fc19982e9c0
habimendoza <habimendoza@hotmail.com>
2019-12-01 20:09:41 -0500

    Updates

---
+++ precache-manifest.deb7c992b092c9985f67ad0025917cd8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04d4201a8e33ce5123c88948ab24f9d",
+    "url": "/expensesapp/index.html"
+  },
+  {
+    "revision": "b9be808ad9a90b4509b4",
+    "url": "/expensesapp/static/css/main.a4f1fe2c.chunk.css"
+  },
+  {
+    "revision": "248c044ea1d5d488efdd",
+    "url": "/expensesapp/static/js/2.2bcfe12b.chunk.js"
+  },
+  {
+    "revision": "b9be808ad9a90b4509b4",
+    "url": "/expensesapp/static/js/main.1efb96e1.chunk.js"
+  },
+  {
+    "revision": "bd1faae3e20f8080f6a3",
+    "url": "/expensesapp/static/js/runtime-main.a1eaf0a9.js"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/expensesapp/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/expensesapp/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/expensesapp/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/expensesapp/static/media/Montserrat-Medium.c8b6e083.ttf"
+  }
+]);
